16be39d46851892e57e107cdcd303b7b4af937f6
Yaroslav <rakshayaroslav37@gmail.com>
2025-03-25 13:57:35 +0200

    m

--- shared_utils/verifyToken.js
+++ shared_utils/verifyToken.js
@@ -3,7 +3,7 @@ const getError = require('./getError');
 
 const verifyToken = (req, res, next) => {
 
-    // check stores list and compare to called :storeId param if it exists then pass user.
+    const { id: storeId } = req.params;
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
@@ -13,6 +13,9 @@ const verifyToken = (req, res, next) => {
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
             return next(getError(401, 'Invalid or expired token.'));
+        }
+        if(!decoded.storeIds.includes(parseInt(storeId))) {
+            return next(getError(401, 'No rights for this store.'))
         }
         req.user = decoded;
         next();
